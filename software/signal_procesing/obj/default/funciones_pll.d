7166fa41b0042b69934bbfa2d472006f0417b4b4
Matias Oliva <mati93_o@hotmail.com>
2025-09-02 14:48:37 -0300

    Limpieza

--- software/signal_procesing/obj/default/funciones_pll.d
+++
@@ -1,3 +0,0 @@
-obj/default/funciones_pll.o: funciones_pll.c funciones_pll.h
-
-funciones_pll.h:
--- software/signal_procesing/obj/default/parameters_control.d
+++
@@ -1,6 +0,0 @@
-obj/default/parameters_control.o: parameters_control.c \
- parameters_control.h mmaped_macros.h
-
-parameters_control.h:
-
-mmaped_macros.h:
